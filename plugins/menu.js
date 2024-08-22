f91954bf5bee4a28c267cb81d4b672ddd9766afd
𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔 𝗦𝗔𝗡𝗗𝗜𝗣𝗔 <166241288+mageekabn@users.noreply.github.com>
2024-08-22 19:08:03 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,62 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "Tharusha md whatsapp bot cmd list.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+  
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `👏 *Hello ${pushname}
+> *DOWNLOAD COMMANDS* ✅
+
+${menu.download}
+
+> *MAIN COMMANDS* ✅
+
+${menu.main}
+
+>*GROUP COMMANDS* ✅
+
+${menu.group}
+
+>*OWNER COMMANDS* ✅
+
+${menu.owner}
+
+>*CONVERT COMMANDS* ✅
+
+${menu.convert}
+
+>*SEARCH COMMANDS* ✅
+
+${menu.search}
+
+POWERD BY THARUSHA SANDIPA 💚
+`
+await conn.sendMessage(from,{text:madeMenu},{quoted:mek})
+
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
