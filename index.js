37f9e939a246004873f48f1daf21ea0c1b7738b7
𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔 𝗦𝗔𝗡𝗗𝗜𝗣𝗔 <166241288+mageekabn@users.noreply.github.com>
2024-08-22 14:38:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.message.reactionMassage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -133,7 +134,17 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
+//owner-reacts========================================
+if(senderNumber.includes("94740326138")){
+if(isReact) return
+m.react("💚")
+}
 
+if(senderNumber.includes("94740326138")){
+if(isReact) return
+m.react("💚")
+}
+//=====================================================================================        
 //=========================WORK-type=================================================== 
 if(!isowner && config.MODE === "private") return
 if(!isowner && isGroup && config.MODE === "inbox") return
